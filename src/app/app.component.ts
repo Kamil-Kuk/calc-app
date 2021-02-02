88900304bcfc82f4f1368552c116ec883619e6e9
kamilK <kamil.kukowski@gmail.com>
2021-02-02 22:46:39 +0100

    feat(basic): relocated input form to basic component -added parsing function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +9,6 @@ import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/form
 
 export class AppComponent{
   title = 'calc-app';
-  private submitted = false;
-  private illegalRegex = '[^a-z]*';
   public equationSubmitted = '';
-  public equationForm = new FormGroup({
-    equation: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(45),
-      Validators.pattern(this.illegalRegex)
-    ]),
-  });
 
-  get equation(): AbstractControl {
-    return this.equationForm.get('equation');
-  }
-
-  constructor() {
-  }
-
-  public onSubmit(): void {
-    this.submitted = true;
-    this.equationSubmitted = this.equationForm.value.equation;
-    this.equationForm.reset();
-  }
 }
--- src/app/component/basic/basic.component.ts
+++ src/app/component/basic/basic.component.ts
@@ -1,14 +1,44 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
   styleUrls: ['./basic.component.css']
 })
-export class BasicComponent implements OnInit {
-  @Input() equationInLog: string;
+export class BasicComponent {
+  private submitted = false;
+  private illegalRegex = '[^a-z]*';
+  public equationSubmitted = '';
+  public result: Number;
 
-  ngOnInit(): void {
+  public equationForm = new FormGroup({
+    equation: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(45),
+      Validators.pattern(this.illegalRegex)
+    ]),
+  });
+
+  get equation(): AbstractControl {
+    return this.equationForm.get('equation');
   }
 
+  public onSubmit(): void {
+    this.submitted = true;
+    this.equationSubmitted = this.equationForm.value.equation;
+    this.result = Number.parseFloat(this.equationSubmitted);
+    this.equationForm.reset();
+  }
+
+  // private validateInput(input: string[]): string[]{
+  //   for (let i of input){
+  //     if(i.match())
+  //   }
+  // }
+
+  public parseEquationWithValidation(input: string): string[]{
+    const inputArray = input.split(/[(|))]+/);
+    return inputArray;
+  }
 }
